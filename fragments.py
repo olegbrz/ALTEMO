cadfffdcec8a665c67d91593b0360cb7aa9fdb0c
Oleg <brezitskyyoleg@gmail.com>
2021-06-21 20:03:37 +0200

    Added histogram function and argument

--- fragments.py
+++ fragments.py
@@ -1,14 +1,23 @@
-from misc.cli_components import DOUBLE_SEPARATOR, SIMPLE_SEPARATOR
-from typing import List, Dict
-from utils.reader import read_fasta
-from utils.math import compute_distances
-from utils.sequence import search_shifted, reverse_complement
 import argparse
+import os
+from datetime import datetime
+from typing import Dict, List
+
+import matplotlib.pyplot as plt
+
+from misc.cli_components import DOUBLE_SEPARATOR, SIMPLE_SEPARATOR
+from utils.math import compute_distances, join_dict_values
+from utils.reader import read_fasta
+from utils.sequence import reverse_complement, search_shifted
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--path',
                     help="path to FASTA (or Multi-FASTA) file",
                     required=True)
+parser.add_argument('--hist',
+                    help="generate histogram of size distribution",
+                    required=False,
+                    action='store_true')
 
 
 def get_fragments(sequence: str) -> Dict[str, List[int]]:
@@ -36,6 +45,26 @@ def get_fragments(sequence: str) -> Dict[str, List[int]]:
     return distances
 
 
+def sizes_histogram(sizes: List[int], seq_name: str):
+    bins = 10
+    plt.figure(figsize=(8, 6))
+    n, bins, patches = plt.hist(sizes, bins=bins, alpha=1, rwidth=0.9)
+    plt.xlabel('Size (bp)', size=14)
+    plt.ylabel('Count', size=14)
+    plt.title(f'Fragment size distribution\n{seq_name}')
+    plt.grid(axis='y', alpha=0.75)
+
+    # Create results folder if doesn't exist
+    if not os.path.exists('results'):
+        os.makedirs('results')
+
+    now = datetime.now()
+    timestamp = now.strftime("%m_%d_%Y_%H_%M_%S_%f")
+    path = f"results/hist_{timestamp}.png"
+    plt.savefig(path)
+    print(f"Histogram saved to {path}")
+
+
 def main():
     args = parser.parse_args()
 
@@ -47,6 +76,10 @@ def main():
         frags = get_fragments(sequences[sequence])
         for f in frags:
             print(f"{f}\t\t\t{frags[f]}")
+
+        if args.hist:
+            print(f"{SIMPLE_SEPARATOR}")
+            sizes_histogram(join_dict_values(frags), sequence)
         print(f"{SIMPLE_SEPARATOR}\n")
 
 
